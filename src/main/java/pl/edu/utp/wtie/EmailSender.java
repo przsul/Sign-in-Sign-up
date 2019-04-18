ae5963f7faeaf159a495f071a334658e68064bef
Przemyslaw Sulecki <przemek.sulecki.mail@gmail.com>
2019-04-18 10:51:49 +0200

    Add imports

--- src/main/java/pl/edu/utp/wtie/EmailSender.java
+++ src/main/java/pl/edu/utp/wtie/EmailSender.java
@@ -1,5 +1,16 @@
 package pl.edu.utp.wtie;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class EmailSender {
 
 }
